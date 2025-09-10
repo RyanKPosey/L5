2b3f57a3d3e8dfae5780fbdb0db107fd8d90cfbe
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-09-10 02:04:59 +0000

    Add main()

--- main.cpp
+++ main.cpp
@@ -4,3 +4,22 @@
     Purpose: Practice using external functions
     Due date: 9/9/2025
 */
+
+#include <iostream>
+#include <array>
+
+int main() {
+    array<int, 5> numbers = {42, 7, 19, 3, 25};
+
+    cout << "Original array: ";
+    for (int num : numbers) cout << num << " ";
+    cout << endl;
+
+    selectionSort(numbers);
+
+    cout << "Sorted array:   ";
+    for (int num : numbers) cout << num << " ";
+    cout << endl;
+
+    return 0;
+}
